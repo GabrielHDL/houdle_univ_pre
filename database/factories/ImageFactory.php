142ea6993285972558a3aeffa5c950c25cff2198
Gabriel91017 <47323206+Gabriel91017@users.noreply.github.com>
2022-02-02 14:09:10 -0600

    dev 1.0.1

---
+++ database/factories/ImageFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Image;
+
+class ImageFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Image::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'url' => 'cursos/' . $this->faker->image('public/storage/cursos', 640, 480, null, false),
+        ];
+    }
+}
