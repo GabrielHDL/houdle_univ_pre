7005d3068bb813bd621cd8b1efc934de6874b196
Gabriel91017 <47323206+Gabriel91017@users.noreply.github.com>
2022-02-19 13:03:45 -0600

    Delete show.blade.php

--- resources/views/courses/certificate/show.blade.php
+++
@@ -1,7 +0,0 @@
-<x-app-layout>
-    Houdle Certifica que: {{auth()->user()->name}}<br>
-    Ha terminado el curso: {{$course->title}}<br>
-    Impartido por: {{$course->teacher->name}}<br>
-    El día:
-    {{$certificate->created_at}}
-</x-app-layout>
